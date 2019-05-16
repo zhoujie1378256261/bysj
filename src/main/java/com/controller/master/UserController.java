ee458cdb95a67050220027ad462d453f3fc089f1
zhoujie <1378256261@qq.com>
2019-05-16 17:54:58 +0800

    5-16

--- src/main/java/com/controller/master/UserController.java
+++ src/main/java/com/controller/master/UserController.java
@@ -23,6 +23,41 @@ public class UserController extends BaseController {
     public void index() {
         render("/weixin/view/index/index.html");
     }
+    public void findUserInfo(){
+        try {
+            int userid = this.getParaToInt("userid");
+            if (userid == 0) {
+                fail_token();
+                return;
+            }
+            success(service.findUserInfo(userid));
+        }catch(Exception e){
+            log.error("", e);
+            fail();
+        }
+    }
+
+    public void updatepwd(){
+        try {
+            int userid = this.getParaToInt("userid");
+            String mm = this.getPara("mm");
+            String xmm = this.getPara("xmm");
+            if (userid == 0) {
+                fail_token();
+                return;
+            }
+            int r = service.updatepwd(userid,mm,xmm);
+            if(r == 0){
+                success(1,"原密码错误");
+            }else {
+                success();
+            }
+
+        }catch(Exception e){
+            log.error("", e);
+            fail();
+        }
+    }
     /**
      * 增加/修改
      */
@@ -72,22 +107,28 @@ public class UserController extends BaseController {
 //            fail();
 //        }
 //    }
+    //登录
     public void login(){
         try {
             String username = this.getPara("username");
             String password = this.getPara("password");
             success(service.login(username, password));
-
         }catch(Exception e){
             log.error("",e);
             fail();
         }
     }
+    //注册
     public void logout(){
         try {
-            String token = this.getPara("token");
-            int userid = JwtUtils.validateJWTForUserId(token);
-            Redis.use().del(REDIS_SYSLOGINKEY_PERFIX+userid);
+            String username = this.getPara("username");
+            String password = this.getPara("password");
+            String gender = this.getPara("gender");
+            Integer deptid = this.getParaToInt("deptid");
+            String position = this.getPara("position");
+            String phone = this.getPara("phone");
+            String email = this.getPara("email");
+            service.logout(username,password,gender,deptid,position,phone,email);
             success();
         }catch(Exception e){
             log.error("", e);
@@ -95,40 +136,5 @@ public class UserController extends BaseController {
         }
     }
 
-    /**
-     * 登陆状态下获取 个人信息
-     */
-//    public void getById(){
-//        SysUser bean = checkLoginStatusForSysUser(this.getPara("token"));
-//        if (bean == null) {
-//            fail_token();
-//            return;
-//        }
-//        int id = this.getParaToInt("id", 0);
-//        if(id==0){
-//            fail();
-//            return;
-//        }
-//        success(service.getById(id));
-//    }
-//    public void delete(){
-//        try {
-//            SysUser bean = checkLoginStatusForSysUser(this.getPara("token"));
-//            if (bean == null) {
-//                fail_token();
-//                return;
-//            }
-//            int id = this.getParaToInt("id", 0);
-//            if (id == 0) {
-//                fail();
-//                return;
-//            }
-//            service.delete(id);
-//            success();
-//        }catch(Exception e){
-//            log.error("", e);
-//            fail();
-//        }
-//    }
 
 }
--- src/main/java/com/service/master/UserService.java
+++ src/main/java/com/service/master/UserService.java
@@ -1,18 +1,15 @@
 package com.service.master;
 
 
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 import com.model.User;
 
 public class UserService {
     public static final UserService me = new UserService();
     private User dao = new User().dao();
 
-    /**
-     * 采购人手机号密码登陆
-     * @param
-     * @param password
-     * @return
-     */
+
     public int login(String username,String password){
 
         User bean = dao.findFirst("select * from t_user where username=? and password=? ",username,password);
@@ -21,6 +18,26 @@ public class UserService {
         }else{
             return 2;
         }
+    }
+
+    public void logout(String username,String password,String gender,Integer deptid,String position,String phone,String email){
+
+        User user = new User();
+        user.setUsername(username).setPassword(password).setGender(gender).setDeptid(deptid).setPosition(position).setPhone(phone).setEmail(email).save();
+    }
+    public Record findUserInfo(int userid){
+        Record r = Db.findFirst("SELECT a.*,b.dept FROM t_user a LEFT JOIN t_dept b ON a.deptid = b.id WHERE a.id = ? ",userid);
+        return r;
+    }
+    public int updatepwd(int userid,String mm,String xmm){
+        Record r = Db.findFirst("SELECT * FROM t_user WHERE id = ? AND `password` = ? ",userid,mm);
+        if(r == null){
+            return 0;
+        }else {
+            Db.update("UPDATE t_user SET `password` = ? WHERE id = ?",xmm,userid);
+            return 1;
+        }
+
     }
 //    public void saveOrUpdate(SysUser bean,int userid){
 //        if(bean.getId() != null && bean.getId()>0){
